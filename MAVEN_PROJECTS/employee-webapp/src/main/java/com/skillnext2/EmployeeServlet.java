41f94ae47ec67d44b895cdd5d18a594e677a4489
varma-2k51 <namrithvarma@gmail.com>
2025-12-23 11:20:27 +0530

    update

---
+++ MAVEN_PROJECTS/employee-webapp/src/main/java/com/skillnext2/EmployeeServlet.java
@@ -0,0 +1,40 @@
+package com.skillnext2;
+
+import com.skillnext2.EmployeeDAO;
+import com.skillnext2.Employee;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class EmployeeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h2>Employee Servlet is WORKING</h2>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        double salary = Double.parseDouble(req.getParameter("salary"));
+
+        Employee emp = new Employee();
+        emp.setName(name);
+        emp.setEmail(email);
+        emp.setSalary(salary);
+
+        EmployeeDAO.save(emp);
+
+        res.sendRedirect("success.jsp");
+    }
+}
